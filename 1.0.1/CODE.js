33e6fc7d95c54b05ed51e772492d9a71b785d8b8
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-06-01 21:32:43 +0300

    Update CODE.js

--- 1.0.1/CODE.js
+++ 1.0.1/CODE.js
@@ -6,7 +6,7 @@
 /*                   */
 /*********************/
 
-jm5 = class jm5class{
+jm5 = new class jm5class{
 
     name = "jM5"
     version = "1.0.1"
@@ -45,22 +45,47 @@ jm5 = class jm5class{
     }
     get(requestURL){
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", requestURL, false );
-    xmlHttp.send( null );
+    xmlHttp.open('GET', requestURL, false);
+    xmlHttp.send(null);
     return xmlHttp.responseText;
     }
+    post(requestURL){
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('POST', requestURL, false);
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    }
+    request(type = "GET", req){
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open(type, requestURL, false);
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    }
     devtools(){
-        return (window.outerHeight - window.innerHeight) > 100)
+    return (window.outerHeight - window.innerHeight > 100)
     }
     removenewlines(string){
         return string.replace('\n', '').trim();
     }
     pushstate(url){
-    return chrome.history.pushState(url)
+    try {
+        return chrome.history.pushState(url)
+    } catch(e){
+        try {
+            return firefox.history.pushState(url)
+        } catch(e){
+            try {
+                return browser.history.pushState(url)
+            } catch(e){
+                // Firefox bug: Call New ( ][<>throw jm5.pushstate()[0]))
+                // `throw jm5.pushstate()[0]`
+                return [false, e]
+            }
+        }
+    }
     }
     islocal(){
-    var proto = location.protocol
-    proto == 'file:')
+    return (location.protocol.includes('file') || location.protocol.includes('localhost'))
     }
     string_eq(string1, string2){
     return (string1 === string2)
@@ -86,7 +111,7 @@ jm5 = class jm5class{
         document.body.removeChild(elementmain);
     }
     __getcode__(version){
-        return this.get(`https://raw.githubusercontent.com/VBPROGER/jm5/main/${version}/CODE-MINIFIED.js`)
+        return (this.get(`https://raw.githubusercontent.com/VBPROGER/jm5/main/${version}/CODE-MINIFIED.js`))
     }
     __getversion__(){
         return (this.get('https://raw.githubusercontent.com/VBPROGER/jm5/main/VER.data').replace('\n', '').trim())
@@ -94,7 +119,7 @@ jm5 = class jm5class{
     __isuptodate__(){
         return (this.__getversion__() == this.version)
     }
-    protocol_set(PROTOCOL){
+    protocol_set(protocol){
         return this.__deprecated__()
     }
     __update__(){
@@ -107,3 +132,10 @@ jm5 = class jm5class{
         throw new Error("This function have been deprecated.")
     }
 }
+
+// Alias
+__jm5proto__ = jm5
+// Detect
+__jm5__ = true
+// Deprecated
+__depr__ = jm5.__deprecated__
